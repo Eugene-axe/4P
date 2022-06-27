92ba1386d36229fb0952dd123331283a1012ec87
Eugene-axe <64367189+Eugene-axe@users.noreply.github.com>
2022-06-27 11:11:09 +0300

    Delete src1 directory

--- src1/index.js
+++
@@ -1,9 +0,0 @@
-import './styles/style.css';
-
-import app from './scripts/app';
-import initial from './scripts/initial';
-import designProtocol from './scripts/designProtocol';
-import handleFile from './scripts/handleFile'
-import footerMenu from './scripts/footermenu'
-import panel from './scripts/panel'
-import table from './scripts/table'
